deb7db5193eb495e03ca530a6c485bd2a79f19a0
KevinVanWezel <21973@ma-web.nl>
2017-05-22 12:35:21 +0200

    first commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class playerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // -1 is left, 0 is no direction, 1 is right
+    public void Move(Vector3 direction)
+    {
+        Vector3 velocity = direction * speed * Time.deltaTime;
+        rb.MovePosition(rb.position + velocity);
+    }
+
+    public void LookAt(Vector3 point)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(point.x,
+                                                    transform.position.y,
+                                                    point.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+}
+
---
+++ Assets/scripts/playerInput.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInput : MonoBehaviour
+{
+
+    private playerMovement _playerMovement;
+    private PlayerShoot _PlayerShoot;
+
+    float xInput;
+
+    void Awake()
+    {
+        _playerMovement = GetComponent<playerMovement>();
+        _PlayerShoot = GetComponent<PlayerShoot>();
+    }
+
+
+
+    void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        //convert pixel coords of ouse to ray.
+        // a ray is an invisible line betweet two points
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * 100);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            print(hit.collider.name);
+            _playerMovement.LookAt(hit.point);
+
+        }
+        if (Input.GetMouseButton(0))
+        {
+            _PlayerShoot.Shoot();
+        }
+        // _playerMovement.LookAt(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        _playerMovement.Move(new Vector3(xInput, 0f, 0f));
+    }
+}
